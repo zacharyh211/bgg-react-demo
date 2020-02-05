fe2f75f4a4f514fbd56f683e379b0a94a1cf311a
Zachary Hancock <zacharyh211@gmail.com>
2020-02-05 05:33:36 -0600

    Updates

---
+++ precache-manifest.34f7bd5ee81013028c542bda144d55c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28555c201166e86d5964cad2271f5271",
+    "url": "/bgg-react-demo/index.html"
+  },
+  {
+    "revision": "f9ea4be551011592b4d8",
+    "url": "/bgg-react-demo/static/css/main.b30ea732.chunk.css"
+  },
+  {
+    "revision": "32e877e625643fda5f0f",
+    "url": "/bgg-react-demo/static/js/2.c3b56a27.chunk.js"
+  },
+  {
+    "revision": "f9ea4be551011592b4d8",
+    "url": "/bgg-react-demo/static/js/main.4a6e186c.chunk.js"
+  },
+  {
+    "revision": "54ed4cac4d3e25788ecb",
+    "url": "/bgg-react-demo/static/js/runtime~main.69934693.js"
+  }
+]);
